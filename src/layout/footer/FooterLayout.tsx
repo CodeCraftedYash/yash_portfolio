7dc7f1b8acdcd0ec29b147eb7e5d5d8d3bfff677
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-22 16:15:05 +0530

    Feat(layout): add footer layout

---
+++ src/layout/footer/FooterLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+
+const FooterLayout: React.FC = () => {
+  return (
+    <div className='w-full min-h-14 bg-[var(--color-surface)] p-4'>
+      <Footer />
+    </div>
+  )
+}
+
+export default FooterLayout
