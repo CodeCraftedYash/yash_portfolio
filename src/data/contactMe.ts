929ccab91e7648a3441f02de4fcc255cd27df117
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-23 14:10:20 +0530

    feat(data): add contact related data

---
+++ src/data/contactMe.ts
@@ -0,0 +1,5 @@
+export const contactMe = [
+  { name: "Email", url: "yash1000mishra@gmail.com" },
+  { name: "phone", url: "6299700545" },
+  { name: "address", url: "Jamshedpur, Jharkhand, India 831011" },
+];
