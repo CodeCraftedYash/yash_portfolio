efc4668b9477eac770d468e38f1a48479694237f
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-23 14:03:11 +0530

    feat(data): add about me data file

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,5 @@
+export const aboutMe = {
+    name: "Yash Mishra",
+    role: "Frontend Developer",
+    description: "Fresh graduate with a passion for creating engaging and user-friendly web applications.Eager to contribute to dynamic teams and grow as a developer.",
+}
