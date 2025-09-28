67428b9d60f0ff36b45877850d6adaccb53a5156
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-28 23:52:07 +0530

    add other component to include elements of the right side in the header

---
+++ src/components/header/other/Other.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeSwitcher from '../otherButtons/ThemeSwitcher'
+
+const Other:React.FC = () => {
+  return (
+    <div className='flex'>
+      <ThemeSwitcher />
+    </div>
+  )
+}
+
+export default Other
