461dc56a2ea83f4a713b952d008a4e5efbb278eb
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-28 23:36:14 +0530

    feat(data): add data for navbar items

---
+++ src/data/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    {name:"Home",to:"/",icon:""},
+    {name:"About",to:"/About",icon:""},
+    {name:"Projects",to:"/project",icon:""},
+    {name:"Contact",to:"/contact",icon:""},
+]
