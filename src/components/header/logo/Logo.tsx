621aae32e9b725cdacc2198446044169a69281de
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-30 23:59:47 +0530

    feat(header): add logo

---
+++ src/components/header/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo:React.FC = () => {
+  return (
+    <div>
+        <h1 className='relative z-50 text-nowrap'>Yash Mishra</h1>
+    </div>
+  )
+}
+
+export default Logo
