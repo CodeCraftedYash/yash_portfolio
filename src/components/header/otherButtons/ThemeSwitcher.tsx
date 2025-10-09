219352ea3e170947b48cbf4e0a94233325ae0484
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-09 22:51:01 +0530

    style(component): change icons for theme switcher

--- src/components/header/otherButtons/ThemeSwitcher.tsx
+++ src/components/header/otherButtons/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { CiLight } from "react-icons/ci";
+import { MdSunny } from "react-icons/md";
 import { MdOutlineNightlight } from "react-icons/md";
 
 const ThemeSwitcher: React.FC = () => {
@@ -18,15 +18,15 @@ const ThemeSwitcher: React.FC = () => {
   };
 
   return (
-    <div className="w-10 overflow-hidden aspect-square cursor-pointer">
+    <div className="w-10 overflow-hidden aspect-square cursor-pointer flex items-center">
       {darkMode ? (
         <MdOutlineNightlight
-          className="text-4xl text-blue-600 hover:cursor-pointer hover:scale-110"
+          className="text-blue-600 hover:cursor-pointer hover:scale-110" style={{fontSize:"var(--icon-size)"}}
           onClick={handleToggle}
         />
       ) : (
-        <CiLight
-          className="text-4xl text-yellow-400 hover:cursor-pointer hover:scale-110"
+        <MdSunny
+          className="text-yellow-500 hover:cursor-pointer hover:scale-110" style={{fontSize:"var(--icon-size)"}}
           onClick={handleToggle}
         />
       )}
