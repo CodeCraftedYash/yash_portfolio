d096f3b1435925132bc59f9df24172575bcab6d8
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-09 22:20:11 +0530

    fix(component): adjusted the style of button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { btnType } from '../../types/buttonType'
 
 const Button:React.FC<btnType> = ({text,cb,customClass,Icon}) => {
-    const baseClass = customClass? customClass : "bg-[var(--color-accent)] px-2 rounded-2xl text-white flex flex-nowrap items-center gap-1 py hover:cursor-pointer hover:scale-110 transition-all duration-175";
+    const baseClass = customClass? customClass : "bg-[var(--color-accent)] px-2 py-3 rounded-2xl text-white flex flex-nowrap items-center gap-1 py hover:cursor-pointer hover:scale-110 transition-all duration-175 h-fit";
   return (
     <button onClick={cb} className={baseClass} style={{fontSize:"var(--font-size-base)"}}>
         <span>{text}</span>
