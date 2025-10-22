9e99000577e999ac6ef62205c55c8ff74b2664b4
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-23 02:19:19 +0530

    fix(components): fix gsap animation , remove slideIn logic and view state , implement timeline reverse and play logic

--- src/components/header/navbar/HambMenu.tsx
+++ src/components/header/navbar/HambMenu.tsx
@@ -4,63 +4,58 @@ import { RxCross2 } from "react-icons/rx";
 import { navItems } from "../../../data/navItems";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { slideIn } from "../../../constants/slideIn";
 
 const HambMenu: React.FC = () => {
   const [flag, setFlag] = useState(false); 
-  const [view, setView] = useState(false); 
   const menuRef: RefObject<HTMLUListElement|null> = useRef(null);
+  const tl = useRef<GSAPTimeline|null>(null);
+  useGSAP(()=>{
+    if(!menuRef.current) return;
+    tl.current = gsap.timeline({paused:true})
+     .fromTo(
+        menuRef.current,
+        { x: 100, opacity: 0 },
+        { x: 0, opacity: 1, duration: 0.3, ease: "power3.out" }
+      );
+  }, []);
 
-  const handleClick = () => {
-    if (flag && menuRef.current && view) {
-      
-      gsap.to(menuRef.current, {
-        ...slideIn("right", 0.6),
-        onComplete: () => setView(false), 
-      });
-    } else {
-      setView(true); 
-    }
-    setFlag((prev) => !prev);
-  };
-
-  useGSAP(
-    () => {
-      if (view && menuRef.current) {
-        gsap.from(menuRef.current, slideIn("right", 0.6));
+    const handleClick = () => {
+      if(!tl.current) return;
+      if(flag){
+        tl.current.reverse();
       }
-    },
-    { dependencies: [view], scope: menuRef }
-  );
-
+      else{
+        tl.current.play();
+      }
+        setFlag(prev=>!prev);
+  }
+  
   return (
-    <nav className="sm:hidden relative">
+    <nav className="sm:hidden relative text-[var(color-text-primary)]">
       {flag ? (
         <RxCross2
           onClick={handleClick}
           className="hover:scale-110 hover:cursor-pointer hover:text-[var(--color-highlight)]"
-          style={{ fontSize: "var(--icon-size)" }}
+          style={{ fontSize: "var(--font-size-icon)" }}
         />
       ) : (
         <MdOutlineMenu
           onClick={handleClick}
           className="hover:scale-110 hover:cursor-pointer hover:text-[var(--color-highlight)]"
-          style={{ fontSize: "var(--icon-size)" }}
+          style={{ fontSize: "var(--font-size-icon)" }}
         />
       )}
 
-      {view && (
         <ul
           ref={menuRef}
-          className="absolute aspect-square w-32 bg-[var(--color-surface)] border-2 top-10 -right-7 p-2 flex flex-col items-left justify-between border-[var(--color-highlight)]"
+          className="absolute aspect-square min-w-32 rounded-xl bg-[var(--color-surface)]/80 top-10 -right-7 p-2 flex flex-col items-left justify-between border-2"
         >
           {navItems.map((item, index) => (
-            <a key={index} href="/">
-              <li className="flex flex-row-reverse flex-nowrap items-center justify-end py-1" style={{fontSize:"var(--font-size-base)"}}>{item.name} <item.icon className="w-6 " /></li>
+            <a key={index} href={item.to}>
+              <li className="flex flex-row-reverse flex-nowrap items-center justify-end py-1 gap-2 my-0.5" style={{fontSize:"var(--font-size-md)"}} >{item.name} <item.icon /></li>
             </a>
           ))}
         </ul>
-      )}
     </nav>
   );
 };
