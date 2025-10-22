cd3ea39f84ed0d17e0ee3e6ef83125dad8385156
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 22:13:26 +0530

    chore(components): fix styles and adjust tag structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,21 +4,18 @@ import Interests from './Interests'
 import ContactMe from './ContactMe'
 
 const Footer: React.FC = () => {
-  const baseContainerClass = 'flex flex-col gap-2 md:w-1/3'
   return (
-    <footer className='flex flex-col '>
-      <div className='flex flex-col md:flex-row gap-8'>
-        <div className={baseContainerClass}>
+    <footer className='flex flex-col text-[var(--color-text-primary)]' style={{fontSize:"var(--font-size-md)"}}>
+      <div className='flex flex-col md:flex-row md:px-20 gap-8 justify-between mb-2'>
+        <div className="md:w-1/3 ">
           <AboutMe />
         </div>
-        <div className={baseContainerClass}>
-          <h2>Interests</h2>
+        <div className="">
           <Interests />
         </div>
-        <div className={baseContainerClass}>
+        <div className="">
           <ContactMe />
         </div>
-
       </div>
       <h3 className='text-center'>
         &copy; {new Date().getFullYear()} Yash Mishra. All rights reserved.
