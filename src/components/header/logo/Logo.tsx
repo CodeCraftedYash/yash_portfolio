eff18cefeae724774922c75e982ae30a426f3baa
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 23:32:56 +0530

    refactor(component): fix styles and adjust tag structure

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo:React.FC = () => {
   return (
     <div>
-        <h1 className='relative z-50 text-nowrap'>Yash Mishra</h1>
+        <h1 className='relative z-50 text-nowrap text-[var(--color-text-primary)]' style={{fontSize:"var(--font-size-icon)"}}>Yash Mishra</h1>
     </div>
   )
 }
