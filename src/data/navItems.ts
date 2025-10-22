376a6897e99c09d868984bc8088581d8089682ce
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 23:37:43 +0530

    feat(data): add icons from react icons to nav items data

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -1,6 +1,11 @@
+import { GoHomeFill } from "react-icons/go";
+import { GoPerson } from "react-icons/go";
+import { SiWikibooks } from "react-icons/si";
+import { PiPhoneCallFill } from "react-icons/pi";
+
 export const navItems = [
-    {name:"Home",to:"/",icon:""},
-    {name:"About",to:"/About",icon:""},
-    {name:"Projects",to:"/project",icon:""},
-    {name:"Contact",to:"/contact",icon:""},
+    {name:"Home",to:"/",icon:GoHomeFill},
+    {name:"About",to:"/About",icon:GoPerson},
+    {name:"Projects",to:"/project",icon:SiWikibooks},
+    {name:"Contact",to:"/contact",icon:PiPhoneCallFill},
 ]
