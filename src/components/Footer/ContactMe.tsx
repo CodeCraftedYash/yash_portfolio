d89d73d2d551ea96d86764f5d31a863282b7b4b8
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 22:04:10 +0530

    feat(component): add contact me component for footer

--- src/components/Footer/ContactMe.tsx
+++ src/components/Footer/ContactMe.tsx
@@ -4,6 +4,7 @@ import { contactMe } from '../../data/contactMe'
 const ContactMe: React.FC = () => {
   return (
     <div className='flex flex-col gap-2'>
+      <h2 className='underline underline-offset-4'>Contact Me</h2>
       {
         contactMe.map((item, index) => (
           <div key={index} className='flex flex-col gap-1'>
