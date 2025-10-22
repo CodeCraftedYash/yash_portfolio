789d2cad6a198ced1d40735e688a90565c38fae5
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 23:36:45 +0530

    feat(components): add icons to the nav items

--- src/components/header/navbar/Navbar.tsx
+++ src/components/header/navbar/Navbar.tsx
@@ -3,12 +3,12 @@ import { navItems } from '../../../data/navItems'
 
 const Navbar:React.FC = () => {
   return (
-    <nav className='w-full hidden sm:block'>
+    <nav className='w-full hidden sm:block text-[var(--color-text-primary)]' style={{fontSize:"var(--font-size-large)"}}>
       <ul className='flex w-fit mx-auto '>
         {navItems.map((item, index) => {
           return (
             <li key={index} className='tracking-wide mx-4 hover:scale-110 hover:text-[var(--color-highlight)] transition-all duration-200 ease-in-out hover:cursor-pointer'>
-              <a><h3>{item.name}</h3></a>
+              <a><h3 className='flex flex-nowrap whitespace-nowrap items-center gap-1'>{item.name} <item.icon /></h3></a>
             </li>
           )
         })}
