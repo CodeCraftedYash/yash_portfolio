e9b40c1c9677f1daba009f7a33240bff1feb3300
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 22:03:36 +0530

    feat(component): add about me component for footer

--- src/components/Footer/AboutMe.tsx
+++ src/components/Footer/AboutMe.tsx
@@ -4,13 +4,13 @@ import { socialMediaLinks } from '../../data/socialMediaLinks'
 
 const AboutMe: React.FC = () => {
     return (
-        <div className='gap-2 flex flex-col'>
-            <h2>{aboutMe.name}</h2>
-            <p className='md:w-[80%]'>{aboutMe.description}</p>
-            <div className='flex space-x-2 mt-2'>
+        <div className='gap-2 flex flex-col items-center'>
+            <h2 className='underline underline-offset-4'>{aboutMe.name}</h2>
+            <p className='text-center'>{aboutMe.description}</p>
+            <div className='flex space-x-2 mt-2 gap-4'>
                 {
                     socialMediaLinks.map((item) => (
-                        <a key={item.name} href={item.url} target='_blank' rel='noopener noreferrer' className=''>
+                        <a key={item.name} href={item.url} target='_blank' rel='noopener noreferrer' className='hover:scale-110 hover:text-[var(--color-highlight)] transition-all duration-200 ease-in-out'>
                             <item.icon style={{
                                 fontSize: "var(--font-size-lg)",
                             }} />
