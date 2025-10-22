541222a3a4182b5e34f18082fcf9527a287b25ee
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-22 23:00:50 +0530

    chore(components): fix styles and adjust tag structure

--- src/components/Footer/Interests.tsx
+++ src/components/Footer/Interests.tsx
@@ -4,6 +4,7 @@ import { interests } from '../../data/interests'
 const Interests:React.FC = () => {
   return (
     <div className='flex flex-col gap-2'>
+       <h2 className='underline underline-offset-4'>Interests</h2>
     {
       interests.map((item,index)=>(
        <h3 key={index}>
