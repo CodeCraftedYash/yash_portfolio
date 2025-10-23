2284d31eff469877870a5a7729be4e3accafaeda
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-23 23:30:27 +0530

    style(components): change h3 tag to h2

--- src/components/header/navbar/Navbar.tsx
+++ src/components/header/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar:React.FC = () => {
         {navItems.map((item, index) => {
           return (
             <li key={index} className='tracking-wide mx-4 hover:scale-110 hover:text-[var(--color-highlight)] transition-all duration-200 ease-in-out hover:cursor-pointer'>
-              <a><h3 className='flex flex-nowrap whitespace-nowrap items-center gap-1'>{item.name} <item.icon /></h3></a>
+              <a><h2 className='flex flex-nowrap whitespace-nowrap items-center gap-1'>{item.name} <item.icon /></h2></a>
             </li>
           )
         })}
