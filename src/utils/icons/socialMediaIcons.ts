71e5334e3da24fcde0219f6bb204df20f50e33ed
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-24 22:36:48 +0530

    feat(utils): add instagram icon

--- src/utils/icons/socialMediaIcons.ts
+++ src/utils/icons/socialMediaIcons.ts
@@ -1,5 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
 
-export { FaGithub, IoMdMail, FaLinkedin };
+export { FaGithub, IoMdMail, FaLinkedin,FaInstagram };
