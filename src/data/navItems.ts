7c078c810f9deddfedffa643174a16588fd356cf
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-24 22:34:01 +0530

    fix(data): replace / with #

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -4,8 +4,8 @@ import { SiWikibooks } from "react-icons/si";
 import { PiPhoneCallFill } from "react-icons/pi";
 
 export const navItems = [
-    {name:"Home",to:"/",icon:GoHomeFill},
-    {name:"About",to:"/About",icon:GoPerson},
-    {name:"Projects",to:"/project",icon:SiWikibooks},
-    {name:"Contact",to:"/contact",icon:PiPhoneCallFill},
+    {name:"Home",to:"#",icon:GoHomeFill},
+    {name:"About",to:"#About",icon:GoPerson},
+    {name:"Projects",to:"#project",icon:SiWikibooks},
+    {name:"Contact",to:"#contact",icon:PiPhoneCallFill},
 ]
