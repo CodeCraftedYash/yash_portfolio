c94f752f8ceb40ae4e78cf60886ff828699049ec
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-24 22:34:58 +0530

    feat(data): add instagram object to the link array data

--- src/data/socialMediaLinks.ts
+++ src/data/socialMediaLinks.ts
@@ -1,8 +1,9 @@
 import type { socialMediaLinksType } from "../types/socialMediaLinksType";
-import { FaGithub, IoMdMail, FaLinkedin } from "../utils/icons/socialMediaIcons";
+import { FaGithub, IoMdMail, FaLinkedin, FaInstagram } from "../utils/icons/socialMediaIcons";
 
 export const socialMediaLinks:socialMediaLinksType[] = [
     { name: 'GitHub', url: 'https://github.com/CodeCraftedYash', icon: FaGithub },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yash-mishra1000/', icon:  FaLinkedin },
     { name: 'Email', url: 'yash1000mishra@gmail.com', icon: IoMdMail },
+    { name: 'Instagram', url: 'https://www.instagram.com/yash_coding/', icon: FaInstagram },
 ]
