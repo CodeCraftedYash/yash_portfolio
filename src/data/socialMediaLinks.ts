aa73755ff3c87a0d1415f960eecfff8cfc16b275
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-25 20:11:23 +0530

    feat(data): add col property

--- src/data/socialMediaLinks.ts
+++ src/data/socialMediaLinks.ts
@@ -2,8 +2,8 @@ import type { socialMediaLinksType } from "../types/socialMediaLinksType";
 import { FaGithub, IoMdMail, FaLinkedin, FaInstagram } from "../utils/icons/socialMediaIcons";
 
 export const socialMediaLinks:socialMediaLinksType[] = [
-    { name: 'GitHub', url: 'https://github.com/CodeCraftedYash', icon: FaGithub },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yash-mishra1000/', icon:  FaLinkedin },
-    { name: 'Email', url: 'yash1000mishra@gmail.com', icon: IoMdMail },
-    { name: 'Instagram', url: 'https://www.instagram.com/yash_coding/', icon: FaInstagram },
+    { name: 'GitHub', url: 'https://github.com/CodeCraftedYash', icon: FaGithub, col:"rgb(38, 39, 38)" },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yash-mishra1000/', icon:  FaLinkedin, col:"rgb(9, 89, 170)" },
+    { name: 'Email', url: 'yash1000mishra@gmail.com', icon: IoMdMail, col:"rgb(120, 3, 3)" },
+    { name: 'Instagram', url: 'https://www.instagram.com/yash_coding/', icon: FaInstagram, col:"rgb(226, 0, 143)" },
 ]
