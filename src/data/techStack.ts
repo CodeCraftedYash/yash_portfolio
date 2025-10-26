35756e7f9d038b92ead67707f4782480a69ecaf4
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-26 15:26:31 +0530

    feat(data): add tech stack items  data

---
+++ src/data/techStack.ts
@@ -0,0 +1,62 @@
+export const techStackData = [
+  {
+    name: "Javascript",
+    img: "https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png",
+    color: "#F0DB4F",
+  }, 
+  {
+    name: "HTML",
+    img: "https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png",
+    color: "#E34F26",
+  }, 
+  {
+    name: "CSS",
+    img: "https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-opencode-css-8.png",
+    color: "#1572B6",
+  },
+  {
+    name: "Tailwind CSS",
+    img: "https://logowik.com/content/uploads/images/tailwind-css3232.logowik.com.webp",
+    color: "#38B2AC",
+  },
+  {
+    name: "ReactJs",
+    img: "https://logos-world.net/wp-content/uploads/2023/08/React-Logo-500x281.png",
+    color: "#61DBFB",
+  },
+  {
+    name: "Typescript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png?20221110153201",
+    color: "#3178C6",
+  },
+  {
+    name: "Git & GitHub",
+    img: "https://i.pinimg.com/474x/7e/12/ce/7e12ceb8f210365adbb8d9ca5ce925f0.jpg",
+    color: "#F05032",
+  },
+  {
+    name: "Motion",
+    img: "https://tsh.io/wp-content/uploads/fly-images/32664/framer-motion-logo-1-312x211.png",
+    color: "#0055FF",
+  },
+  {
+    name: "GSAP",
+    img: "https://logowik.com/content/uploads/images/greensock-gsap4425.logowik.com.webp",
+    color: "#88CE02",
+  },
+  {
+    name: "Node.js",
+    img: "https://images.seeklogo.com/logo-png/27/1/node-js-logo-png_seeklogo-273749.png",
+    color: "#339933",
+  },
+  {
+    name: "Express.js",
+    img: "https://images.seeklogo.com/logo-png/33/1/express-js-logo-png_seeklogo-339850.png",
+    color: "#000000",
+  },
+  {
+    name: "Mongoose",
+    img: "https://logodix.com/logo/1590965.png",
+    color: "#880000",
+  },
+];
