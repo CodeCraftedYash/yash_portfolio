fbd0903fe126a3c92a435e069863b3e7dd0d3197
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-27 03:35:10 +0530

    feat(page): implement scroll smoother using gsap

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import HeaderLayout from '../layout/header/HeaderLayout'
-import HomeLayout from '../layout/home/HomeLayout'
-import FooterLayout from '../layout/footer/FooterLayout'
+import React, { useRef } from "react";
+import HeaderLayout from "../layout/header/HeaderLayout";
+import HomeLayout from "../layout/home/HomeLayout";
+import FooterLayout from "../layout/footer/FooterLayout";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useGSAP } from "@gsap/react";
 
-const HomePage:React.FC = () => {
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
+
+const HomePage: React.FC = () => {
+  const main = useRef(null);
+  const smoother = useRef<ScrollSmoother | null>(null);
+
+  useGSAP(
+    () => {
+      smoother.current = ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 2,
+        effects: true,
+      });
+      return () => {
+        smoother.current?.kill();
+      };
+    },
+    { scope: main }
+  );
   return (
-    <div className=' flex flex-col '>
-    <HeaderLayout />
-    <HomeLayout />
-    <FooterLayout />
+    <div id="smooth-wrapper" ref={main}>
+      <div
+        className="flex flex-col  bg-[var(--color-background)]"
+        id="smooth-content"
+      >
+        <HeaderLayout />
+        <HomeLayout />
+        <FooterLayout />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
