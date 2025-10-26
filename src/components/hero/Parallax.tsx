21d6739d0a931ee65a66da446673e20875f991b9
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-10-27 05:29:08 +0530

    feat(components): add a parallax component for hero section

---
+++ src/components/hero/Parallax.tsx
@@ -0,0 +1,27 @@
+import gsap from "gsap";
+import { techStackData } from "../../data/techStack";
+
+const Parallax = () => {
+  const icons = techStackData.map((item, index) => {
+    const random = gsap.utils.random(1.3, 1.5);
+
+    return (
+      <div key={index} className="border-2" data-speed={random}>
+        <img
+          src={item.img}
+          alt={item.name}
+          className="w-12 aspect-square object-contain"
+          style={{ backgroundColor: item.color }}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <div className="relative w-full h-[60vh] overflow-hidden grid place-items-center grid-cols-3 gap-4">
+      {icons}
+    </div>
+  );
+};
+
+export default Parallax;
